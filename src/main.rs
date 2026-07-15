fa7431845e187b079901853ce3c468de09379c98
don <onellaquino@icloud.com>
2026-07-15 10:26:49 -0400

    Generated basic image

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let image_width = 256;
+    let image_height = 256;
+    
+    println!("P3\n{image_width} {image_height}\n235");
+
+    for j in 0..image_height{
+        for i in 0..image_width{
+            let r = i as f64 / (image_width -1) as f64;
+            let g = j as f64 / (image_height -1) as f64;
+            let b = 0.0;
+
+            let ir = (255.999 * r)as usize;
+            let ig = (255.999 * g)as usize;
+            let ib = (255.999 * b)as usize;
+
+            println!("{ir} {ig} {ib}");
+        }
+    }
+
+}
