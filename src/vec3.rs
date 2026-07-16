9a6ba2b3232a12fc90a44302a209af8b89aa6c16
don <onellaquino@icloud.com>
2026-07-15 22:04:57 -0400

    Created vec3 header file

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,3 +1,12 @@
-imp vec3{
+#[derive(Clone, Copy, Default)]
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64
+}
 
+impl Vec3{
+    pub fn new(x : f64, y : f64, z :f64) -> Vec3{
+        Vec3{x, y, z}
+    }
 }
