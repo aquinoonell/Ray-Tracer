1229890c2445279b2b326c58f6ce57b80ae63836
don <onellaquino@icloud.com>
2026-07-20 23:22:46 -0400

    Surface Normals and Multiple Objects

---
+++ src/sphere.rs
@@ -0,0 +1,48 @@
+use crate::hittable::{HitRecord, Hittable};
+use crate::ray::Ray;
+use crate::vec3::{self, Point3, Vec3};
+
+
+pub struct Sphere{
+    center: Point3,
+    radius: f64,
+}
+
+impl Sphere {
+    pub fn new(cen: Point3, r: f64) -> Sphere {
+        Sphere {
+            center: cen,
+            radius: r,
+        }
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let oc = r.origin() - self.center;
+        let a = r.direction().length_squared();
+        let half_b = vec3::dot(oc, r.direction());
+        let c = oc.length_squared() - self.radius * self.radius;
+        let discriminat = half_b * half_b - a * c;
+        if discriminat < 0.0 {
+            return false;
+        }
+
+        let sqrt_d = f64::sqrt(discriminat);
+
+        // Find the nearest root that lies in the acceptable range
+        let mut root = (-half_b - sqrt_d) / a;
+        if root <= t_min  || t_max <= root {
+            root = (-half_b + sqrt_d) / a;
+            if root <= t_min || t_max <= root {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        let outward_normal = (rec.p - self.center) / self.radius;
+        rec.set_face_normal(r, outward_normal);
+        true
+    }
+}
