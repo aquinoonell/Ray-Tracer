d80583bb0fb80598109339797556da6128a01eaf
don <onellaquino@icloud.com>
2026-07-22 22:24:41 -0400

    Different color Spheres

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use camera::Camera;
 use colors::Color;
 use hittable::{HitRecord, Hittable};
 use hittable_list::HittableList;
-use material::{Lambertian, Metal};
+use material::{Lambertian,Dielectric, Metal};
 use rand::Rng;
 use ray::Ray;
 use sphere::Sphere;
@@ -73,8 +73,8 @@ fn main() {
     let mut world = HittableList::new();
 
     let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
-    let material_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8)));
+    let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let material_left = Rc::new(Dielectric::new(1.5));
     let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2)));
 
     world.add(Box::new(Sphere::new(
