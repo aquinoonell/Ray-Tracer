f7f6b3f83416a019abf855c88f1f45b258d68467
don <onellaquino@icloud.com>
2026-07-27 05:39:55 -0400

    Final Scene bug cleaning

--- src/main.rs
+++ src/main.rs
@@ -127,9 +127,9 @@ fn main() {
     // Image
 
     const ASPECT_RATIO: f64 = 3.0 / 2.0;
-    const IMAGE_WIDTH: i32 = 400; //1200;
+    const IMAGE_WIDTH: i32 =  1200;
     const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
-    const SAMPLES_PER_PIXEL: i32 = 50; //500;
+    const SAMPLES_PER_PIXEL: i32 = 500;
     const MAX_DEPTH: i32 = 50;
 
     //  World
